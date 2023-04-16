57250239854dea00adbaef986bbff747592c94fe
Johannes Weigelt <jweigelt@mail.uni-paderborn.de>
2023-04-16 21:01:40 +0200

    Made the Desktop user page look more 'editor-like'.

---
+++ src/js/object-control-desktop.component.js
@@ -0,0 +1,86 @@
+﻿/* global AFRAME, NAF */
+AFRAME.registerComponent('object-control-desktop', {
+  schema: {
+    template: { default: '' },
+    keyCode: { default: 32 }
+  },
+
+  init: function() {
+    // EasyRTC listener for object spawner
+    var that = this;
+    
+    easyrtc.setServerListener( function(msgType, msgData, targeting) {
+
+      console.log("Received server msg: " + msgType);
+
+      if(msgType === 'spawnComponent') {
+        var newData = JSON.parse(msgData);
+        that.spawnEntity(newData);
+
+      }else if(msgType === 'selectedComponent') {
+        var data = JSON.parse(msgData);
+        var cid = data.cid;
+        var bool = data.bool;
+        // @ToDo: add true/false
+        that.selectEntity(cid, bool);
+      }
+    });
+    
+
+    /*
+    easyrtc.addEventListener("roomOccupant", (eventName, eventData) => {
+    */
+  },
+
+  spawnEntity: function(data) {
+      var shape = data.shape;
+      var el = document.createElement('a-entity');
+
+      // Handle geometry/shape attribute
+      if(shape == 'sphere') {
+        el.setAttribute('geometry', 'primitive: sphere; segmentsWidth: 16; segmentsHeight: 16');
+      }else if(shape == 'plane') {
+        el.setAttribute('geometry', 'primitive: plane;');
+        el.setAttribute('rotation', {x: -90, y: 0, z: 0});
+      }else{
+        el.setAttribute('geometry', 'primitive: ' + shape + ';');
+      }
+
+      // Handle other attributes
+      el.setAttribute('material', 'color: blue');
+      el.setAttribute('selectable', true)
+      
+      el.setAttribute('cid', data.cid);
+
+      var pos3 = new THREE.Vector3(data.posX, data.posY, data.posZ);
+
+      el.setAttribute('position', pos3);
+      var scene = this.el.sceneEl;
+      scene.appendChild(el);
+  },
+
+  selectEntity: function(componentId, bool) {
+
+      var el = null;
+      var els = this.el.sceneEl.querySelectorAll('[cid]');
+
+      for (var i = 0; i < els.length; i++) {
+
+        if(els[i].getAttribute('cid') === componentId) {
+          el = els[i];
+          break;
+        }
+      }
+
+      // Decide whether to select or deselect the object
+      if(bool) {
+        // Select object
+        el.setAttribute('material', 'color: red');
+
+      }else{
+        // Deselect object
+        el.setAttribute('material', 'color: blue');
+      }
+  }
+});
+
